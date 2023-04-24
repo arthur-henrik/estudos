e6897d6638bb210db0045d983fbd4db1e71a3398
Arthur henrique <128714636+arthur-henrik@users.noreply.github.com>
2023-04-24 12:10:29 -0300

    Add files via upload

---
+++ Turma A/Aula7/array e arrays.py
@@ -0,0 +1,14 @@
+a = []
+m = []
+for y in range(10):
+    a.append(float(input("digite o numero: ")))
+
+x = int(input("digite numero para multiplicar: "))
+
+for z in range(10):
+    m.append(x * a[z])
+
+
+print(a)
+print(x)
+print(m)
